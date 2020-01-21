ac7977f48632281608c53d7cecd82a8f72a0500a
B. programming <34455022+b-programming@users.noreply.github.com>
2020-01-21 02:19:56 +0100

    Update product.route.js

--- routes/product.route.js
+++ routes/product.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const product_controller = require('../controllers/product.controller');
 
 
-// a simple test url to check that all of our files are communicating correctly.
+// a simple test url
 //router.get('/test', product_controller.test);
 
 router.post('/create', product_controller.product_create);
